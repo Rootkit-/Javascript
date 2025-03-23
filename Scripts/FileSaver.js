166ec1ac0699f46b82deeb372ff528d9f5c28b90
Hoecat <sexyjake@gmail.com>
2025-03-23 12:27:34 -0500

    a
    a

--- Scripts/FileSaver.js
+++ Scripts/FileSaver.js
@@ -83,93 +83,37 @@ function click(node) {
 
 var isMacOSWebView = _global.navigator && /Macintosh/.test(navigator.userAgent) && /AppleWebKit/.test(navigator.userAgent) && !/Safari/.test(navigator.userAgent)
 
-var saveAs = _global.saveAs || (
-    // probably in some web worker
-    (typeof window !== 'object' || window !== _global) ?
-    function saveAs() {
-        /* noop */
-    }
+function saveAs(blob, name, opts) {
+    var URL = _global.URL || _global.webkitURL
+    // Namespace is used to prevent conflict w/ Chrome Poper Blocker extension (Issue #561)
+    var a = document.createElementNS('http://www.w3.org/1999/xhtml', 'a')
+    name = name || blob.name || 'download'
 
-    // Use download attribute first if possible (#193 Lumia mobile) unless this is a macOS WebView
-    :
-    ('download' in HTMLAnchorElement.prototype && !isMacOSWebView) ?
-    function saveAs(blob, name, opts) {
-        var URL = _global.URL || _global.webkitURL
-        // Namespace is used to prevent conflict w/ Chrome Poper Blocker extension (Issue #561)
-        var a = document.createElementNS('http://www.w3.org/1999/xhtml', 'a')
-        name = name || blob.name || 'download'
+    a.download = name
+    a.rel = 'noopener' // tabnabbing
 
-        a.download = name
-        a.rel = 'noopener' // tabnabbing
+    // TODO: detect chrome extensions & packaged apps
+    // a.target = '_blank'
 
-        // TODO: detect chrome extensions & packaged apps
-        // a.target = '_blank'
-
-        if (typeof blob === 'string') {
-            // Support regular links
-            a.href = blob
-            if (a.origin !== location.origin) {
-                corsEnabled(a.href) ?
-                    download(blob, name, opts) :
-                    click(a, a.target = '_blank')
-            } else {
-                click(a)
-            }
+    if (typeof blob === 'string') {
+        // Support regular links
+        a.href = blob
+        if (a.origin !== location.origin) {
+            corsEnabled(a.href) ?
+                download(blob, name, opts) :
+                click(a, a.target = '_blank')
         } else {
-            // Support blobs
-            a.href = URL.createObjectURL(blob)
-            setTimeout(function() {
-                URL.revokeObjectURL(a.href)
-            }, 4E4) // 40s
-            setTimeout(function() {
-                click(a)
-            }, 0)
+            click(a)
         }
+    } else {
+        // Support blobs
+        a.href = URL.createObjectURL(blob)
+        setTimeout(function() {
+            URL.revokeObjectURL(a.href)
+        }, 4E4) // 40s
+        setTimeout(function() {
+            click(a)
+        }, 0)
     }
-
-    // Fallback to using FileReader and a popup
-    :
-    function saveAs(blob, name, opts, popup) {
-        // Open a popup immediately do go around popup blocker
-        // Mostly only available on user interaction and the fileReader is async so...
-        popup = popup || open('', '_blank')
-        if (popup) {
-            popup.document.title =
-                popup.document.body.innerText = 'downloading...'
-        }
-
-        if (typeof blob === 'string')
-            return download(blob, name, opts)
-
-        var force = blob.type === 'application/octet-stream'
-        var isSafari = /constructor/i.test(_global.HTMLElement) || _global.safari
-        var isChromeIOS = /CriOS\/[\d]+/.test(navigator.userAgent)
-
-        if ((isChromeIOS || (force && isSafari) || isMacOSWebView) && typeof FileReader !== 'undefined') {
-            // Safari doesn't allow downloading of blob URLs
-            var reader = new FileReader()
-            reader.onloadend = function() {
-                var url = reader.result
-                url = isChromeIOS ? url : url.replace(/^data:[^;]*;/, 'data:attachment/file;')
-                if (popup)
-                    popup.location.href = url
-                else
-                    location = url
-                popup = null // reverse-tabnabbing #460
-            }
-            reader.readAsDataURL(blob)
-        } else {
-            var URL = _global.URL || _global.webkitURL
-            var url = URL.createObjectURL(blob)
-            if (popup)
-                popup.location = url
-            else
-                location.href = url
-            popup = null // reverse-tabnabbing #460
-            setTimeout(function() {
-                URL.revokeObjectURL(url)
-            }, 4E4) // 40s
-        }
-    })
-
+}
 _global.saveAs = saveAs
